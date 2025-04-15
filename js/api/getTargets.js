1e6285d9a5c0e071f1028a184b9b86b27e1a4755
klu0926 <lukuoyu@gmail.com>
2025-04-14 20:21:27 -0400

    chore: remove some log

--- js/api/getTargets.js
+++ js/api/getTargets.js
@@ -5,11 +5,8 @@ export async function getTargets(count = 87) {
   try {
     const oldTargets = getLocalTargets()
     if (oldTargets) {
-      console.log('getTarget: use old targets')
       return oldTargets
     }
-    console.log('getTarget: fetch new targets...')
-
     // use random user api to get targets
     const response = await fetch(`https://randomuser.me/api/?results=${count}`);
     const res = await response.json();
@@ -21,7 +18,6 @@ export async function getTargets(count = 87) {
     // store target
     setLocalTargets(targets)
 
-    console.log('Successfully get targets:', res.results);
     return targets;
   } catch (err) {
     console.error('[ERROR] getTargets:', err.message)
